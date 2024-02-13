5433453bc9257ff21951a3ed1d5dbfef7553ddb6
JeramyBosquezDominguez <146022970+JeramyBosquezDominguez@users.noreply.github.com>
2024-02-13 23:11:10 +0100

    Update beforeinstall.sh

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,2 +1,12 @@
-  sudo chmod +x gradlew
-  ./gradlew war
+#!/bin/bash
+cd /opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
+
+sudo chmod +x gradlew
+
+./gradlew war
+
+cd ./build/libs
+
+name=`ls`
+
+mv $name hola.war
